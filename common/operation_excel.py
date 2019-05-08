b78b20686f2645a7d3f8805ccd5c31925307658e
macheng5 <1140945843@qq.com>
2019-05-08 11:24:22 +0800

    no message

--- common/operation_excel.py
+++ common/operation_excel.py
@@ -36,6 +36,14 @@ class OperationExcel:
             data_dict = self.table.loc[i].to_dict()
             data.append(data_dict)
         return data
+	def get_data_info(self):
+        """获取表格详细信息"""
+        data = []
+        for i in self.table.index.values:
+            data_dict = self.table.loc[i].to_dict()
+            data.append(data_dict)
+        return data
+
 
 if __name__ == '__main__':
     operation = OperationExcel('../data/test_data.xlsx')
